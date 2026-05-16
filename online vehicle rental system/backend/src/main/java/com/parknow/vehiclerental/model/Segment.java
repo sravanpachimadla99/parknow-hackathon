fb27123035726d47525e4df3c8f624bfa7f28cad
sravanpachimadla99 <24eg505t03@anurag.edu.in>
2026-05-16 23:27:04 +0530

    Delete online vehicle rental system directory

--- online vehicle rental system/backend/src/main/java/com/parknow/vehiclerental/model/Segment.java
+++
@@ -1,8 +0,0 @@
-package com.parknow.vehiclerental.model;
-
-public enum Segment {
-    ECONOMY,
-    COMPACT,
-    PREMIUM,
-    LUXURY
-}
--- online vehicle rental system/backend/src/main/java/com/parknow/vehiclerental/repository/VehicleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.parknow.vehiclerental.repository;
-
-import com.parknow.vehiclerental.model.Vehicle;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
-}
